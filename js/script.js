886c19587fc8602892ebc5c60b97aecb8959e616
Eugene <ToaIgnika@users.noreply.github.com>
2018-10-18 11:23:44 -0700

    test

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-  // Initialize Firebase
-  var config = {
+//Create an account on Firebase, and use the credentials they give you in place of the following
+var config = {
     apiKey: "AIzaSyAeP0mIZv7syPdQDQf4tSklyW4xbR86Erg",
     authDomain: "ignika-79b0b.firebaseapp.com",
     databaseURL: "https://ignika-79b0b.firebaseio.com",
     projectId: "ignika-79b0b",
-    storageBucket: "",
+    storageBucket: "ignika-79b0b.appspot.com",
     messagingSenderId: "112520978396"
   };
-  firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 var database = firebase.database().ref();
 var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
-var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}]};
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
 var pc = new RTCPeerConnection(servers);
 pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
 pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
@@ -52,12 +52,4 @@ function showFriendsFace() {
   pc.createOffer()
     .then(offer => pc.setLocalDescription(offer) )
     .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
-}
-
-function copy_link() {
-  $('#hidden_url').val(window.location.href);
-  $('#hidden_url').select();
-  var successful = document.execCommand('copy');  
-  var msg = successful ? 'successful' : 'unsuccessful';  
-  console.log('Copy link: ' + msg);  
 }
