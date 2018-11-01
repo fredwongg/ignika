dd667043d2ed612749d079cbec7309e0a774f37e
Eugene <ToaIgnika@users.noreply.github.com>
2018-11-01 16:08:56 -0700

    addStream issue fixed

--- js/script.js
+++ js/script.js
@@ -15,13 +15,14 @@ var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
 const servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
+var myStream;
 
 var connection_list = [];
 function createConnection() {
     let pc = new RTCPeerConnection(servers);
     pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
     pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
-    
+    pc.addStream(myStream);
     connection_list.push(pc);
 }
 createConnection();
@@ -53,7 +54,7 @@ database.on('child_added', readMessage);
 function showMyFace() {
   navigator.mediaDevices.getUserMedia({audio:true, video:true})
     .then(stream => yourVideo.srcObject = stream)
-    .then(stream => pc.addStream(stream));
+    .then(stream => myStream = stream);
 }
 
 function showFriendsFace() {
