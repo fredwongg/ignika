d0cd1e2f14e7df03ef10192ff38b36d4f4979ed8
Eugene <ToaIgnika@users.noreply.github.com>
2018-11-01 16:06:39 -0700

    creating a new function for connections

--- js/script.js
+++ js/script.js
@@ -14,10 +14,17 @@ var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
-var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
-var pc = new RTCPeerConnection(servers);
-pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+const servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
+
+var connection_list = [];
+function createConnection() {
+    let pc = new RTCPeerConnection(servers);
+    pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+    pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+    
+    connection_list.push(pc);
+}
+createConnection();
 
 function sendMessage(senderId, data) {
     var msg = database.push({ sender: senderId, message: data });
@@ -27,6 +34,7 @@ function sendMessage(senderId, data) {
 function readMessage(data) {
     var msg = JSON.parse(data.val().message);
     var sender = data.val().sender;
+    pc = connection_list[0];
     if (sender != yourId) {
         if (msg.ice != undefined)
             pc.addIceCandidate(new RTCIceCandidate(msg.ice));
@@ -49,6 +57,7 @@ function showMyFace() {
 }
 
 function showFriendsFace() {
+    let pc = connection_list[0];
   pc.createOffer()
     .then(offer => pc.setLocalDescription(offer) )
     .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
