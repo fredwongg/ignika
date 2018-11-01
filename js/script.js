ec0700ef89dcea208e1787b16d5deaeaa41ea15f
Eugene <ToaIgnika@users.noreply.github.com>
2018-11-01 13:42:43 -0700

    hhh

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ function readMessage(data) {
     //console.log(data);
     let msg = JSON.parse(data.val().message);
     var sender = data.val().sender;
-    console.log(msg + "|" + sender);
+    console.log(msg);
     if (sender != yourId) {
         let pc = getConnection(sender);
         if (msg.ice != undefined) {
