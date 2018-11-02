568211a484fdc6f98f654bf36048223b6e8920ca
Eugene <ToaIgnika@users.noreply.github.com>
2018-11-01 17:41:35 -0700

    trying hardcode

--- js/script.js
+++ js/script.js
@@ -42,13 +42,8 @@ function readMessage(data) {
     var msg = JSON.parse(data.val().message);
     var sender = data.val().sender;
     //pc = getConnection(sender);
-   
+    pc = connection_list[0];
     if (sender != yourId) {
-        let x = user_list.indexOf(sender);
-        console.log(sender);
-        console.log(user_list);
-        console.log(x);
-        pc = connection_list[x];
         if (msg.ice != undefined)
             pc.addIceCandidate(new RTCIceCandidate(msg.ice));
         else if (msg.sdp.type == "offer")
